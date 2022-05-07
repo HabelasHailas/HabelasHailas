7cf164078891ac119872724eb1f15fa746cf0ec9
u1968408 <99675267+u1968408@users.noreply.github.com>
2022-05-07 07:32:35 +0200

    Update game.js
    Movimiento mejorado con con menos errores. Animacion de ataque añadida.

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -23,6 +23,7 @@ class GameScene extends Phaser.Scene{
 
         //captura entrada del teclado
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         //#region ANIMACIONES JUGADOR
         this.anims.create({
@@ -43,37 +44,104 @@ class GameScene extends Phaser.Scene{
             frameRate: 7,
             repeat: -1
         });
+        this.anims.create({
+            key:'attack',
+            frames: this.anims.generateFrameNumbers('character',{start: 12, end: 15}),
+            frameRate: 7
+        });
+        this.anims.create({
+            key:'death',
+            frames: this.anims.generateFrameNumbers('character',{start: 24, end: 26}),
+            frameRate: 5
+        });
         //#endregion
       
     }
 
-    update(){
-
+    updatePlayer(){
         //#region INPUT TECLADO 
-        if (this.cursors.right.isDown){
+
+        // Mates detrás de los numeros:
+        // https://www.geogebra.org/calculator/qbbr4vb7
+
+        if (this.spacebar.isDown) {
+            this.player.anims.play('attack',true);
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+        }
+        else if (this.cursors.right.isDown && this.cursors.up.isUp
+            && this.cursors.down.isUp && this.cursors.left.isUp){
+            // Derecha
             this.player.setVelocityX(160);
+            this.player.setVelocityY(0);
             this.player.anims.play('right',true);
             this.player.flipX = false;
         }
-        if (this.cursors.up.isDown){
+        else if (this.cursors.right.isDown && this.cursors.up.isDown
+            && this.cursors.down.isUp && this.cursors.left.isUp){
+            // Arriba + Derecha
+            this.player.setVelocityX(113);
+            this.player.setVelocityY(-113);
+            this.player.anims.play('right',true);
+            this.player.flipX = false;
+        }
+        else if (this.cursors.right.isUp && this.cursors.up.isDown
+            && this.cursors.down.isUp && this.cursors.left.isUp){
+            // Arriba
+            this.player.setVelocityX(0);
             this.player.setVelocityY(-160);
             this.player.anims.play('right',true);
         }
-        if (this.cursors.down.isDown){
-            this.player.setVelocityY(160);
-            this.player.anims.play('right',true);
-        }
-        if (this.cursors.left.isDown){
-            this.player.setVelocityX(-160);
+        else if (this.cursors.right.isUp && this.cursors.up.isDown
+            && this.cursors.down.isUp && this.cursors.left.isDown){
+            // Arriba + Izquierda
+            this.player.setVelocityX(-113);
+            this.player.setVelocityY(-113);
             this.player.anims.play('left',true);
             this.player.flipX = true;
         }
-        if(this.cursors.right.isUp && this.cursors.up.isUp
-             && this.cursors.down.isUp && this.cursors.left.isUp) { //mirar que no se pulse ninguna de las 4 teclas direccionales
+        else if (this.cursors.right.isUp && this.cursors.up.isUp
+            && this.cursors.down.isUp && this.cursors.left.isDown){
+            // Izquierda
+            this.player.setVelocityX(-160);
+            this.player.setVelocityY(0);
+            this.player.anims.play('left',true);
+            this.player.flipX = true;
+        }
+        else if (this.cursors.right.isUp && this.cursors.up.isUp
+            && this.cursors.down.isDown && this.cursors.left.isDown){
+            // Abajo + Izquierda
+            this.player.setVelocityX(-113);
+            this.player.setVelocityY(113);
+            this.player.anims.play('left',true);
+            this.player.flipX = true;
+        }
+        else if (this.cursors.right.isUp && this.cursors.up.isUp
+            && this.cursors.down.isDown && this.cursors.left.isUp){
+            // Abajo
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(160);
+            this.player.anims.play('right',true);
+        }
+        else if (this.cursors.right.isDown && this.cursors.up.isUp
+            && this.cursors.down.isDown && this.cursors.left.isUp){
+            // Abajo + Derecha
+            this.player.setVelocityX(113);
+            this.player.setVelocityY(113);
+            this.player.anims.play('right',true);
+        }
+        else { //mirar que no se pulse ninguna de las 4 teclas direccionales
+            // if(this.cursors.right.isUp && this.cursors.up.isUp && this.cursors.down.isUp && this.cursors.left.isUp) 
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
             this.player.anims.play('idle', true);
         }
-        //#endregion
+    }
+
+    update(){
+
+        this.updatePlayer();
+
     }
 }
+
