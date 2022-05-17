2cb513ab073348223a83c765bbb83b7031be13ac
u1968408 <99675267+u1968408@users.noreply.github.com>
2022-05-17 15:30:05 +0200

    Update player.js

--- js/scenes/player.js
+++ js/scenes/player.js
@@ -1,22 +1,23 @@
 "use strict"
 
-class Player{
+class Player {
     key_a = null;
     key_s = null;
     key_d = null;
     key_w = null;
     key_e = null;
-    constructor(context){
+    constructor(context) {
         this.context = context;
         this.player = null;
     }
-    preloadPlayer(){
-        this.context.load.spritesheet('characterIdle','../../sprites/character/B_witch_idle.png', {frameWidth: 32, frameHeight: 48});
-        this.context.load.spritesheet('characterWalk','../../sprites/character/B_witch_run.png', {frameWidth: 32, frameHeight: 48});
-        this.context.load.spritesheet('characterAttack','../../sprites/character/B_witch_attack2.png', {frameWidth: 48, frameHeight: 48});
+    preloadPlayer() {
+        this.context.load.spritesheet('characterIdle', '../../sprites/character/B_witch_idle.png', { frameWidth: 32, frameHeight: 48 });
+        this.context.load.spritesheet('characterWalk', '../../sprites/character/B_witch_run.png', { frameWidth: 32, frameHeight: 48 });
+        this.context.load.spritesheet('characterAttack', '../../sprites/character/B_witch_attack2.png', { frameWidth: 48, frameHeight: 48 });
+        this.context.load.spritesheet('characterAttack', '../../sprites/character/B_witch_attack_projectile.png', { frameWidth: 48, frameHeight: 48 });
     }
-    createPlayer(){
-        this.player = this.context.physics.add.sprite(100,450,'characterIdle').setScale(2).refreshBody();
+    createPlayer() {
+        this.player = this.context.physics.add.sprite(100, 450, 'characterIdle').setScale(2).refreshBody();
         // this.player.setBox(100);
         this.player.setCollideWorldBounds(true);
 
@@ -25,30 +26,36 @@ class Player{
         this.context.key_d = this.context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.context.key_w = this.context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.context.key_e = this.context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+        this.context.key_f = this.context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 
-         //#region ANIMACIONES JUGADOR
-         this.context.anims.create({
-            key:'right',
-            frames: this.context.anims.generateFrameNumbers('characterWalk',{start: 0, end: 7}),
+        //#region ANIMACIONES JUGADOR
+        this.context.anims.create({
+            key: 'right',
+            frames: this.context.anims.generateFrameNumbers('characterWalk', { start: 0, end: 7 }),
             frameRate: 10,
             repeat: -1
         });
         this.context.anims.create({
-            key:'left',
-            frames: this.context.anims.generateFrameNumbers('characterWalk',{start: 0, end: 7}),
+            key: 'left',
+            frames: this.context.anims.generateFrameNumbers('characterWalk', { start: 0, end: 7 }),
             frameRate: 10,
             repeat: -1
         });
         this.context.anims.create({
-            key:'idle',
-            frames: this.context.anims.generateFrameNumbers('characterIdle',{start: 0, end: 5}),
+            key: 'idle',
+            frames: this.context.anims.generateFrameNumbers('characterIdle', { start: 0, end: 5 }),
             frameRate: 7,
             repeat: -1
         });
         this.context.anims.create({
-            key:'attack',
-            frames: this.context.anims.generateFrameNumbers('characterAttack',{start: 0, end: 8}),
-            frameRate: 7            
+            key: 'attack',
+            frames: this.context.anims.generateFrameNumbers('characterAttack', { start: 0, end: 8 }),
+            frameRate: 5
+        });
+        this.context.anims.create({
+            key: 'attackProj',
+            frames: this.context.anims.generateFrameNumbers('characterAttackProjectile', { start: 0, end: 10 }),
+            frameRate: 5
         });
         // this.context.anims.create({
         //     key:'death',
@@ -58,68 +65,75 @@ class Player{
         //#endregion
 
     }
-    updatePlayer(){        
+    updatePlayer() {
         // Mates detrás de los numeros:
         // https://www.geogebra.org/calculator/qbbr4vb7
 
         if (this.context.key_e.isDown) {
             // Ataque
-            this.player.anims.play('attack',true);
+            this.player.anims.play('attack', true);
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+            console.log(this.player.body.getOffset())
+        }
+        else if (this.context.key_f.isDown) {
+            // Proyectil ataque
+            this.player.anims.play('attackProj', true);
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
         }
-        else if (this.context.key_d.isDown && this.context.key_w.isUp && this.context.key_s.isUp && this.context.key_a.isUp){
+        else if (this.context.key_d.isDown && this.context.key_w.isUp && this.context.key_s.isUp && this.context.key_a.isUp) {
             // Derecha
             this.player.setVelocityX(160);
             this.player.setVelocityY(0);
-            this.player.anims.play('right',true);
+            this.player.anims.play('right', true);
             this.player.flipX = false;
         }
-        else if (this.context.key_d.isDown && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isUp){
+        else if (this.context.key_d.isDown && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isUp) {
             // Arriba + Derecha
             this.player.setVelocityX(113);
             this.player.setVelocityY(-113);
-            this.player.anims.play('right',true);
+            this.player.anims.play('right', true);
             this.player.flipX = false;
         }
-        else if (this.context.key_d.isUp && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isUp){
+        else if (this.context.key_d.isUp && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isUp) {
             // Arriba
             this.player.setVelocityX(0);
             this.player.setVelocityY(-160);
-            this.player.anims.play('right',true);
+            this.player.anims.play('right', true);
         }
-        else if (this.context.key_d.isUp && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isDown){
+        else if (this.context.key_d.isUp && this.context.key_w.isDown && this.context.key_s.isUp && this.context.key_a.isDown) {
             // Arriba + Izquierda
             this.player.setVelocityX(-113);
             this.player.setVelocityY(-113);
-            this.player.anims.play('left',true);
+            this.player.anims.play('left', true);
             this.player.flipX = true;
         }
-        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isUp && this.context.key_a.isDown){
+        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isUp && this.context.key_a.isDown) {
             // Izquierda
             this.player.setVelocityX(-160);
             this.player.setVelocityY(0);
-            this.player.anims.play('left',true);
+            this.player.anims.play('left', true);
             this.player.flipX = true;
         }
-        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isDown){
+        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isDown) {
             // Abajo + Izquierda
             this.player.setVelocityX(-113);
             this.player.setVelocityY(113);
-            this.player.anims.play('left',true);
+            this.player.anims.play('left', true);
             this.player.flipX = true;
         }
-        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isUp){
+        else if (this.context.key_d.isUp && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isUp) {
             // Abajo
             this.player.setVelocityX(0);
             this.player.setVelocityY(160);
-            this.player.anims.play('right',true);
+            this.player.anims.play('right', true);
         }
-        else if (this.context.key_d.isDown && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isUp){
+        else if (this.context.key_d.isDown && this.context.key_w.isUp && this.context.key_s.isDown && this.context.key_a.isUp) {
             // Abajo + Derecha
             this.player.setVelocityX(113);
             this.player.setVelocityY(113);
-            this.player.anims.play('right',true);
+            this.player.anims.play('right', true);
             this.player.flipX = false;
         }
         else { //mirar que no se pulse ninguna de las 4 teclas direccionales
